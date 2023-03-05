9a8ef9b6526b77768acf8c09f93531ffd42d6aac
Robinson Presotto <robipresotto@gmail.com>
2023-03-05 17:14:18 +0100

    Setup terraform

---
+++ Scripts/terraform/prometheus.tf
@@ -0,0 +1,31 @@
+resource "helm_release" "prometheus" {
+  chart      = "prometheus"
+  name       = "prometheus"
+  namespace  = var.namespace
+  repository = "https://prometheus-community.github.io/helm-charts"
+  version    = "19.7.2"
+
+  set {
+    name  = "podSecurityPolicy.enabled"
+    value = true
+  }
+
+  set {
+    name  = "server.persistentVolume.enabled"
+    value = false
+  }
+
+  set {
+    name = "server\\.resources"
+    value = yamlencode({
+      limits = {
+        cpu    = "200m"
+        memory = "50Mi"
+      }
+      requests = {
+        cpu    = "100m"
+        memory = "30Mi"
+      }
+    })
+  }
+}
