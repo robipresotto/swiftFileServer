49296ff54979ba5bb17e16a097cabef0d9e558e9
Robinson Presotto <robipresotto@gmail.com>
2023-03-25 13:07:33 +0100

    Fix minio-testkube credentials

--- Scripts/terraform/testkube.tf
+++ Scripts/terraform/testkube.tf
@@ -1,17 +1,3 @@
-resource "kubernetes_secret" "testkube" {
-  metadata {
-    name = "testkube"
-    namespace = "testkube"
-  }
-
-  data = {
-    admin-user = "admin"
-    admin-password = random_password.testkube.result
-  }
-}
-resource "random_password" "testkube" {
-  length = 24
-}
 resource "helm_release" "testkube" {
   chart            = "testkube"
   name            = "testkube"
@@ -21,10 +7,13 @@ resource "helm_release" "testkube" {
 
   values = [
     templatefile("${path.module}/templates/testkube-values.yaml", {
-      admin_existing_secret = kubernetes_secret.testkube.metadata[0].name
       telemetry = false
-      minioRootUser = "admin-user"
-      minioRootPassword = "admin-password"
+      minioAccessKeyId = kubernetes_secret.minio.data.rootUser
+      minioAccessKey = kubernetes_secret.minio.data.rootPassword
     })
+  ]
+
+  depends_on = [
+    helm_release.minio
   ]
 }
