e4419f5319ef1d2fe3cbf84a005d4d9734420234
Robinson Presotto <robipresotto@gmail.com>
2023-03-25 21:54:06 +0100

    Cleanup prometheus config

--- Scripts/terraform/prometheus.tf
+++ Scripts/terraform/prometheus.tf
@@ -1,31 +1,14 @@
 resource "helm_release" "prometheus" {
-  chart      = "prometheus"
-  name       = "prometheus"
+  chart            = "prometheus"
+  name            = "prometheus"
   namespace  = "monitoring"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  version    = "19.7.2"
+  repository    = "https://prometheus-community.github.io/helm-charts"
+  version         = "19.7.2"
 
-  set {
-    name  = "podSecurityPolicy.enabled"
-    value = true
-  }
-
-  set {
-    name  = "server.persistentVolume.enabled"
-    value = false
-  }
-
-  set {
-    name = "server\\.resources"
-    value = yamlencode({
-      limits = {
-        cpu    = "200m"
-        memory = "50Mi"
-      }
-      requests = {
-        cpu    = "100m"
-        memory = "30Mi"
-      }
+  values = [
+    templatefile("${path.module}/templates/prometheus-values.yaml", {
+      persistentVolume = true
     })
-  }
+  ]
+
 }
